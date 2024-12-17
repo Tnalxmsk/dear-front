5b838b433205e2538519b5c5856138a8ab8bd06e
Tnalxmsk <alstnwkd990@naver.com>
2024-12-17 16:08:43 +0900

    :art: style: HomePage 화면 구현

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,20 @@
 import styled from 'styled-components';
 import Header from '../components/common/Header.tsx';
+import Card from '../components/home/Card.tsx';
 
 const HomePage = () => {
+
+  const nickname = '콩';
+
   return (
     <Container>
       <Header />
       <Inner>
-        Test
+        <Greeting>{nickname}님 반갑습니다.</Greeting>
+        <CardContainer>
+          <Card label="편지쓰기" />
+          <Card label="우편함" />
+        </CardContainer>
       </Inner>
     </Container>
   );
@@ -24,9 +32,24 @@ const Container = styled.div`
 `;
 
 const Inner = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   margin-top: 78px;
   font-size: 30px;
   width: 100vw;
+  gap: 86px;
   height: 723px;
-  background-color: ${({ theme }) => theme.colors.black35};
+  background-color: ${({ theme }) => theme.colors.black15};
+  padding-top: 13vh;
+`;
+
+const Greeting = styled.div`
+  font: ${({ theme }) => theme.fonts.heading_regular_32px};
+  color: black;
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  gap: 200px;
 `;
