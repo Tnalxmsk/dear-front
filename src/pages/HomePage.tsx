d497dfd01fc89cb49ab26fd6cc5ebcce6b11c49c
Tnalxmsk <alstnwkd990@naver.com>
2024-12-17 17:42:17 +0900

    :recycle: refactor: #2-home CSS 모듈 분리 및 컴포넌트 분리

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,13 @@
 import styled from 'styled-components';
 import Header from '../components/common/Header.tsx';
-import Card from '../components/home/Card.tsx';
+import CardWidget from '../components/home/CardWidget.tsx';
 
 const HomePage = () => {
-
-  const nickname = '콩';
 
   return (
     <Container>
       <Header />
-      <Inner>
-        <Greeting>{nickname}님 반갑습니다.</Greeting>
-        <CardContainer>
-          <Card label="편지쓰기" />
-          <Card label="우편함" />
-        </CardContainer>
-      </Inner>
+        <CardWidget />
     </Container>
   );
 };
@@ -29,27 +21,5 @@ const Container = styled.div`
   text-align: center;
   width: 100%;
   height: 100vh;
-`;
-
-const Inner = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 78px;
-  font-size: 30px;
-  width: 100vw;
-  gap: 86px;
-  height: 723px;
-  background-color: ${({ theme }) => theme.colors.black15};
   padding-top: 13vh;
-`;
-
-const Greeting = styled.div`
-  font: ${({ theme }) => theme.fonts.heading_regular_32px};
-  color: black;
-`;
-
-const CardContainer = styled.div`
-  display: flex;
-  gap: 200px;
 `;
