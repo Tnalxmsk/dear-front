b11adcdff53edeb8f95b177b1400a4401ae51c1b
Tnalxmsk <alstnwkd990@naver.com>
2024-12-17 16:07:20 +0900

    :art: style: Card 컴포넌트 구현

---
+++ src/components/home/Card.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+interface CardProps {
+  label: string;
+}
+
+const Card = ({ label }: CardProps) => {
+  return (
+    <CardContainer>
+      <CardImage />
+      <Title>{label}</Title>
+    </CardContainer>
+  );
+};
+
+export default Card;
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  width: 145px;
+  height: 160px;
+  gap: 20px;
+  border: none;
+  padding: 20px 30px 15px 30px;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+  background-color: white;
+`;
+
+const CardImage = styled.div`
+  width: 85px;
+  height: 85px;
+  border-radius: 10px;
+  border: none;
+  background: ${({ theme }) => theme.colors.black100} url('/src/assets/letter.svg') no-repeat center;
+`;
+
+const Title = styled.div`
+  text-align: center;
+  font: ${({ theme }) => theme.fonts.paragraph_regular_14px};
+  color: black;
+`
